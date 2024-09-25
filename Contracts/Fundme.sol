da780d25fa8cba4e411b0497f35c83cf3a552f4d
xxBRAHMOSxx <devanshkumar6298@gmail.com>
2024-09-25 16:34:09 +0530

    conversion and minimum amount of eth

--- Contracts/Fundme.sol
+++ Contracts/Fundme.sol
@@ -6,16 +6,28 @@ import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interf
 
 contract FundMe{
 
+    uint256 minValueUsd = 5e18;
+
     function fund() public payable  {
 
-        require(msg.value > 1e18,"didnt sent rnough eth");
+        require(getConversionRate(msg.value) > minValueUsd,"didnt sent rnough eth");
     }
 
-    function getPrice() public {
+    function getPrice() public view returns(uint256) {
 
         //0x694AA1769357215DE4FAC081bf1f309aDC325306
-            }
-    function getConversionRate() public {}
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (/*uint80 rountInt*/, int256 price,/* uint256 storedAt*/, /*uint256 timeStamp*/, /*uint80 answeredInRound */) = priceFeed.latestRoundData();
+        //price of eth in usd
+        //returns 2000.00000000
+        //doesnt work well with decimals hence we added 1e10 i.e. 10**10 or 10,000,000,000
+        return uint256(price*1e10);
+    }
+    function getConversionRate(uint256 ethAmount) public view returns(uint256) {
+        uint256 ethPrice = getPrice();  
+        uint256 ethAmountInUsd = (ethPrice*ethAmount) / 1e18;
+        return ethAmountInUsd;
+    }
 
     function getVersion() public view returns(uint256){
         return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
